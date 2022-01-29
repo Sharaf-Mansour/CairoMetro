aa3ed55c8c937be61254b224778963c5d9bd4e4c
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2022-01-30 00:48:16 +0200

    Create _Imports.cs

---
+++ _Imports.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using CairoMetro;
+global using System.Collections.Generic;
+global using System;
+global using System.Text.RegularExpressions;
+global using System.Text.Json;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components;
+global using System.Net.Http.Json;
+
+namespace CairoMetro;
