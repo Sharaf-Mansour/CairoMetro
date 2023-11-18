27ee9d0f7ee6d8cab88dff1e8d6b30fe86ab2735
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-11-18 20:05:33 +0200

    Fix Style

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -2,7 +2,6 @@ namespace CairoMetro.Pages;
 public partial class Index
 {
     Station? CurrentStatin { get; set; }
-
     bool clicked = false;
     void Click(Station item) => (CurrentStatin, clicked) = CurrentStatin != item ? (item, true) : (CurrentStatin, !clicked);
     void MouseOver(Station item) => CurrentStatin = clicked ? CurrentStatin : item;
